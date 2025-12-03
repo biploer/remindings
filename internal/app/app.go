ed88b7367bf742f65341a7c707ad80035fd1b76d
Leonid Krapchatov <hellsing1998nsk@gmail.com>
2025-12-03 21:51:42 +0300

    Add "Hello world" server

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,11 +2,35 @@ package app
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
 )
 
 func Run(ctx context.Context) error {
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello, fellow!")
+	})
+
+	server := http.Server{
+		Addr:         "0.0.0.0:8080",
+		Handler:      router,
+		IdleTimeout:  60 * time.Second,
+		ReadTimeout:  4 * time.Second,
+		WriteTimeout: 4 * time.Second,
+	}
+
 	go func() {
 		<-ctx.Done()
+		log.Println("Server is shutting down...")
+		server.Shutdown(ctx)
 	}()
+
+	log.Println("Server is starting...")
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
 	return nil
 }
